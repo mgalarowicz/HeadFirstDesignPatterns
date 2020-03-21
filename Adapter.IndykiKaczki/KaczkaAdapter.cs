fe294771ebb288d111d4de41a3c91c8e345c15bb
mgalarowicz <marek.galarowicz@enova.pl>
2020-03-21 10:42:33 +0100

    Adapter p2

---
+++ Adapter.IndykiKaczki/KaczkaAdapter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adapter.IndykiKaczki
+{
+    public class KaczkaAdapter : IIndyk
+    {
+        private readonly IKaczka _kaczka;
+        private Random _rand;
+
+        public KaczkaAdapter(IKaczka kaczka)
+        {
+            _kaczka = kaczka;
+            _rand = new Random();
+        }
+
+        public void Gulgocz()
+        {
+            _kaczka.Kwacz();
+        }
+
+        public void Lataj()
+        {
+            if (_rand.Next(5) == 0)
+                _kaczka.Lataj();
+        }
+    }
+}
